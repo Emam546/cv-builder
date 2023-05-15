4a56f8a9befdcaa7a799150df5cf9965e26357dc
Emam546 <emam54637@gmail.com>
2023-05-15 17:54:19 +0200

    remove unwanted charchters

--- server/src/util/user.ts
+++ server/src/util/user.ts
@@ -5,10 +5,23 @@ export interface Options {
     provider_id: string;
     provider_type: UserProvider["provider_type"];
 }
+function removeSuspiciousCharacters(key: string) {
+    // Define a regular expression pattern to match suspicious characters
+    const pattern = /[^A-Za-z0-9\-_.~]/g;
+
+    // Remove suspicious characters from the key using the replace method
+    const sanitizedKey = key.replaceAll(pattern, "");
+
+    return sanitizedKey;
+}
 export async function createUser(props: Options) {
     const newUser = new Users({
         ...props,
-        apiKey: generateApiKey(),
+        apiKey: removeSuspiciousCharacters(
+            generateApiKey({
+                dashes: false,
+            }) as string
+        ),
     });
     const res = await newUser.save();
     return res;
